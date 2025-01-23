0421c68570b305f3db9b9f688ec80da3a9cf8f92
Rohan Kumar Sharma <rkrk02233@gmail.com>
2025-01-24 00:44:36 +0530

    updated

--- data/cart.js
+++ data/cart.js
@@ -1,14 +1,26 @@
-export let cart = [{
-  productId:
-   'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-   quantity: 2,
-},{
+export let cart = JSON.parse(localStorage.getItem('cart'));
 
-  productId:
-  '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity: 1
+    if(!cart)
+    {
+      cart = [{
+        productId:
+         'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+         quantity: 2,
+      },{
+      
+        productId:
+        '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 1
+      
+      }];
+    }
+  
 
-}];
+
+function saveToStorage()
+{
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addtocart(productId)
 {
@@ -35,6 +47,8 @@ export function addtocart(productId)
       quantity: 1
     });
   }
+
+   saveToStorage();
 }
 
 export function removeFromCart(productId)
@@ -50,6 +64,7 @@ export function removeFromCart(productId)
   });
 
    cart = newCart;
+   saveToStorage();
 }
 
     
